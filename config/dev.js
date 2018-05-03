0a9b1ae5732f1ae6adde9622209ee518bc783ed6
unknown <psasa@gmail.com.com>
2018-05-03 15:43:39 +0200

    first commit

---
+++ config/dev.js
@@ -0,0 +1,13 @@
+// dev.js, don't commit this!
+
+module.exports = {
+    googleClientID: '564196785922-34vdoog4nsmon5vs1dhhfn33jtdbbppo.apps.googleusercontent.com',
+    googleClientSecret: 'OqZqJKzdPksjpN3o5UtWWU6L',
+    googleCallback: '/auth/google/callback',
+    FACEBOOK_APP_ID: '1632992416769481',
+    FACEBOOK_APP_SECRET: 'f9bb9bfff9c4c327916ca7261abb31d1',
+    FACEBOOK_APP_CALLBACK: 'https://shrouded-coast-13620.herokuapp.com/auth/facebook/callback',
+    mongoURI: 'mongodb://mujo:zehrin@ds239137.mlab.com:39137/kolekcija-vina',
+    cookieKey: 'grkakndghbkuanbgnbllabn'
+}
+
